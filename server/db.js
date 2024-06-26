80d9acb15fc0b9648c326ad93c3cfa7c8c2ea026
Victor-Weng <VictorWengVTW@gmail.com>
2024-06-26 16:26:05 +0800

    test

--- server/db.js
+++ server/db.js
@@ -1,8 +1,28 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
+// Log environment variables to debug
+console.log('POSTGRES_URL:', process.env.POSTGRES_URL);
+
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL,
-})
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+pool.on('error', (err) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
+});
+
+pool.connect((err, client, done) => {
+  if (err) {
+    console.error('Error connecting to the database', err);
+  } else {
+    console.log('Connected to the PostgreSQL database');
+    done(); // release the client back to the pool
+  }
+});
 
 module.exports = pool;
--- server/index.js
+++ server/index.js
@@ -67,7 +67,7 @@ app.get("/bites/:id", async(req,res) => {
   
 
 // set the port from environment variable or default to 5000
-const port = process.env.POSTGRES_PORT || 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log("server has started on port "+ port.toString());
